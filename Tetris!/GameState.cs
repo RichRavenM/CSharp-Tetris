a0eab946a75defc0825bc1eb2330a95cf28ae3fe
RichRavenM <richard.s.morris@btinternet.com>
2023-10-31 16:02:09 +0000

    writes initial gamestate methods

---
+++ Tetris!/GameState.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris_
+{
+    public class GameState
+    {
+        private Block currentBlock;
+
+        public Block CurrentBlock
+        {
+            get => currentBlock;
+            private set
+            {
+                currentBlock = value;
+                currentBlock.Reset();
+            }
+
+        }
+
+        public GameGrid GameGrid { get; }
+        public BlockQueue BlockQueue { get; }
+        public bool GameOver { get; private set; }
+
+        public GameState()
+        {
+            GameGrid = new GameGrid(22, 10);
+            BlockQueue = new BlockQueue();
+            CurrentBlock = BlockQueue.GetAndUpdate();
+        }
+
+        private bool BlockFits()
+        {
+            foreach (Position p in CurrentBlock.TilePositions())
+            {
+                if (!GameGrid.IsEmpty(p.Row, p.Col))
+                {
+                    return false;
+                }
+
+            }
+
+            return true;
+        }
+
+        public void RotateBlockClockwise()
+        {
+            CurrentBlock.RotateClockwise();
+
+            if (!BlockFits())
+            {
+                CurrentBlock.RotateAntiClockwise();
+            }
+        }
+
+        public void RotateBlockAntiClockwise()
+        {
+            CurrentBlock.RotateAntiClockwise();
+
+            if (!BlockFits())
+            {
+                CurrentBlock.RotateClockwise();
+            }
+        }
+
+        public void MoveBlockLeft()
+        {
+            CurrentBlock.Move(0, -1);
+
+            if (!BlockFits())
+            {
+                CurrentBlock.Move(0, 1);
+            }
+        }
+
+        public void MoveBlockRight()
+        {
+            CurrentBlock.Move(0, 1);
+
+            if (!BlockFits())
+            {
+                CurrentBlock.Move(0, -1);
+            }
+        }
+
+        private bool IsGameOver()
+        {
+            if (GameGrid.IsRowEmpty(0) && GameGrid.IsRowEmpty(1))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private void PlaceBlock()
+        {
+            foreach (Position p in CurrentBlock.TilePositions())
+            {
+                GameGrid[p.Row, p.Col] = CurrentBlock.Id;
+            }
+
+            GameGrid.ClearFullRows();
+
+            if (IsGameOver())
+            {
+                GameOver = true;
+            }
+            else
+            {
+                CurrentBlock = BlockQueue.GetAndUpdate();
+            }
+        }
+
+        public void MoveBlockDown()
+        {
+            CurrentBlock.Move(1, 0);
+
+            if(!BlockFits())
+            {
+                CurrentBlock.Move(-1, 0);
+                PlaceBlock();
+            }
+        }
+    }
+}
